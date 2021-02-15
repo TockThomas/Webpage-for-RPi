e2b01065f3831b36b4b905c32c58ee6ec88f2354
TockThomas <thomas.schleicher@fastad.de>
2021-02-15 15:13:15 +0100

    Created Webpage

---
+++ Webpage for RPi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webpage_for_RPi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
